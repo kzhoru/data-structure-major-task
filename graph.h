db67ed290a4387339a6d83a017c45be554031cc2
kzhoru <adrianputraperkasa16@gmail.com>
2024-12-02 01:06:16 +0700

    create function searchVertex and createVertex

--- graph.h
+++ graph.h
@@ -7,8 +7,10 @@
 #define nextVertex(V) V->next
 #define posisi(V) V->posisi
 #define jmlKunjungan(V) V->jmlKunjungan
-
 #define firstEdge(V) V->firstEdge
+#define destVertexID(E) E->destVertexID
+#define weight(E) E->weight
+#define nextEdge(E) E->nextEdge
 using namespace std;
 
 typedef struct vertex *adrVertex;
@@ -41,6 +43,10 @@ void addVertex_103022330008(graph &G, char newVertexID);
 void buildGraph_103022330008(graph &G);
 
 void showVertex(graph G);
+
+adrVertex searchVertex(graph G, char vertexID);
+
+void createEdge(graph &G,char vertexId,char destVertexId, int weight);
 
 
 #endif // GRAPH_H_INCLUDED
